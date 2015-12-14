6a9f34ed45565e746c0ff71d38880c70e6115b5f
mgzeitouni <mgzeitouni@gmail.com>
2015-12-14 12:14:50 -0500

    Add Bing Search API Functionality

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from datetime import datetime
+from bing_search import run_query
 
 def index(request):
 
@@ -249,3 +250,17 @@ def restricted(request):
 # #
 # #     # Take the user back to the homepage.
 # #     return HttpResponseRedirect('/rango/')
+
+
+def search(request):
+
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render(request, 'rango/search.html', {'result_list': result_list})
